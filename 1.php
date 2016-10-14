eb60ba1f91141e80fe6dc10f757d26676f84c5d3
Alexandr Kozlovsky <258464@gmail.com>
2016-10-14 09:38:50 +0700

    123

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+echo ("Now dir: ".getcwd()."\r\n");
+$handle = opendir('Desktop/1c/');
+while (false !== ($entry = readdir($handle))) {
+        if ($entry=='1.php' OR $entry=='..' OR $entry=='.') continue;
+		if (!file_exists("Desktop/1c/$entry/1Cv7.CFG")) continue;
+		$file=file_get_contents("Desktop/1c/$entry/1Cv7.CFG");
+		if (substr_count($file, 'ÎáìåíÅÃÀÈÑ_ôëÍåÓäàëÿòüÇàïðîñûÈçÓÒÌ')>0) {
+				$file=str_replace('{""ÎáìåíÅÃÀÈÑ_ôëÍåÓäàëÿòüÇàïðîñûÈçÓÒÌ"",""{""""N"""",""""0"""",""""0"""",""""0"""",""""0"""",""""0"""",""""1""""}""}', '{""ÎáìåíÅÃÀÈÑ_ôëÍåÓäàëÿòüÇàïðîñûÈçÓÒÌ"",""{""""N"""",""""0"""",""""0"""",""""0"""",""""0"""",""""0"""",""""0""""}""}', $file);
+				file_put_contents("Desktop/1c/$entry/1Cv7.CFG", $file);
+				echo ("TRUE $entry\r\n");
+			}
+		else {
+				echo ("FALSE $entry\r\n");
+		}
+    }
+?>
